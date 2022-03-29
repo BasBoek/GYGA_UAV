808c9b9727b2260ea817bdf5ab453aa8b3a6812a
Boekelo <bastiaen.boekelo@wur.nl>
2022-03-29 16:00:50 +0200

    Neat

--- 00_Preprocess_orthos_CK.R
+++ 00_Preprocess_orthos_CK.R
@@ -1,10 +1,16 @@
 # Bastiaen Boekelo, April 2021
 # Nebraska project - UAV and Oil Palm Nutrients
-# Goal: Restore UAV image of 
+# Goal: Write single band rasters to intermediate folder
 
-rm(list=ls())  # Clean script <- clean mind
 
+      ##########################################
+      #### CHECK WD's & ADAPT IF NECESSARY #####
+      ##########################################
+
+      
 # Set Script and Data wd
+rm(list=ls())  
+   
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Works if you have a recent version of RStudio.
 wd <- "C:/Users/boeke015/OneDrive - Wageningen University & Research/UAV_Palm/Data/"
 
@@ -14,10 +20,9 @@ ras_names <- list.files(paste0(wd, "1_Input/UAV/CK/2b_Reflectance_CK"), pattern=
 
 for(i in 1:length(ras_names)){
   ras <- stack(paste0(wd, "1_Input/UAV/CK/2b_Reflectance_CK/", ras_names[i]))
-  ras <- round(ras * 32768)
+  ras <- round(ras * 32768) # Comparable with original format, see Micasense site
   
   writeRaster(ras, paste0(wd, "1_Input/UAV/CK/2_Orthophotos_CK/", ras_names[1]), format="GTiff", overwrite=TRUE, datatype='INT4S')
-  #writeRaster(ras, ras_names[i], format="GTiff", overwrite=TRUE, datatype='INT4S')
-  
+
 }
 
